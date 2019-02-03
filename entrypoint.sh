d243772f28f0b69e6f5e41a1e6e83d8681847fd9
Angel Nunez Mencias <git@angelnu.com>
2019-02-03 12:49:35 +0100

    cache is a folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ export APACHE_LOG_DIR=/tmp
 export APACHE_PID_FILE=/tmp/apache.pid
 export APACHE_RUN_USER=www-data
 export APACHE_RUN_GROUP=www-data
+
+mkdir -p /cache/musicindex
+chmod 777 /cache/musicindex
 chown -R $APACHE_RUN_USER:$APACHE_RUN_GROUP /cache
 exec apache2 -X
